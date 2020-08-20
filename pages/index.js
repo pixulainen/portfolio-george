e056dc4f4f2011c2f8f4f6c2ed755c1226df5120
pixulainen <george.apetrei@gmail.com>
2020-08-20 15:37:04 +0100

    production testing

--- pages/index.js
+++ pages/index.js
@@ -74,8 +74,8 @@ const Index = () => {
 							<Col md="6" className="hero-welcome-wrapper">
 								<div className="hero-welcome-text">
 									<h1>
-										Welcome to the portfolio website of Filip Jerga. Get informed, collaborate and
-										discover projects I was working on through the years!
+										Welcome to the portfolio website of George Apetrei. Get informed, collaborate
+										and discover projects I was working on through the years!
 									</h1>
 								</div>
 								<Typed
